1a34a4777ab614708266f19ef0ec2504007e5723
ifisher18 <ifisher18@LAB-SCI-214-16>
2016-10-25 22:14:15 -0700

    Initial Commit

---
+++ CS172_HW05_EX05_02 Is Sorted/CS172_HW05_EX05_02 Is Sorted/Source.cpp
@@ -0,0 +1,15 @@
+//////////////////////
+// Ian Fisher
+// CS 172
+// 10/25/16
+//////////////////////
+
+#include <iostream>
+using namespace std;
+
+// Write function to check if the elements in the array are sorted
+// test the function
+
+int main() {
+	return 0;
+}
