9b52768cfc77a0eb482f83c0baf80a159c343e09
ifisher18 <ifisher18@LAB-SCI-214-16>
2016-10-25 22:20:06 -0700

    Initial Commit

---
+++ CS172_HW05_EX05_04 Shuffle Vector/CS172_HW05_EX05_04 Shuffle Vector/Source.cpp
@@ -0,0 +1,15 @@
+//////////////////////////
+// Ian Fisher
+// CS 172
+// 10/25/16
+//////////////////////////
+
+#include <iostream>
+using namespace std;
+
+// Write a function that shuffles the contents in a vector
+// write test program...
+
+int main() {
+	return 0;
+}
