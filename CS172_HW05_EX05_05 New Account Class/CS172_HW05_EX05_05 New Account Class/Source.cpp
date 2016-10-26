0b79fa5c36068b1f97dc1a3d3bee9184add5af9e
ifisher18 <ifisher18@LAB-SCI-214-16>
2016-10-25 22:22:48 -0700

    Initial Commit

---
+++ CS172_HW05_EX05_05 New Account Class/CS172_HW05_EX05_05 New Account Class/Source.cpp
@@ -0,0 +1,15 @@
+//////////////////////
+// Ian Fisher
+// CS 172
+// 10/25/16
+//////////////////////
+
+#include <iostream>
+using namespace std;
+
+// Modify the Account class
+// write test program...
+
+int main() {
+	return 0;
+}
