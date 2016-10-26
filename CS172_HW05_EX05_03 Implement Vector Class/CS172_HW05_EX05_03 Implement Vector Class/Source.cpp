343b7ccd488a1ae0930159924dab931bbe26aefb
ifisher18 <ifisher18@LAB-SCI-214-16>
2016-10-25 22:17:19 -0700

    Initial Commit

---
+++ CS172_HW05_EX05_03 Implement Vector Class/CS172_HW05_EX05_03 Implement Vector Class/Source.cpp
@@ -0,0 +1,15 @@
+//////////////////////////
+// Ian Fisher
+// CS 172
+// 10/25/16
+//////////////////////////
+
+#include <iostream>
+using namespace std;
+
+// Implement the vector class as an exercise
+// implement only the functions defined in the UML class diagram, as shown in Figure 12.2
+
+int main() {
+	return 0;
+}
