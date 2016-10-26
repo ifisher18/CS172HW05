1a52ea9d17ea9d4216439fd7e1b30165f2ea87d0
ifisher18 <ifisher18@LAB-SCI-214-16>
2016-10-25 22:11:07 -0700

    Initial Commit

---
+++ CS172_HW05_EX05_01 Linear Search/CS172_HW05_EX05_01 Linear Search/Source.cpp
@@ -0,0 +1,14 @@
+//////////////////////////
+// Ian Fisher
+// CS 172
+// 10/25/16
+//////////////////////////
+
+#include <iostream>
+using namespace std;
+
+// rewrite the linear search function in Listing 7.9 to use a generic type for array elements.
+// test the function...
+int main() {
+	return 0;
+}
