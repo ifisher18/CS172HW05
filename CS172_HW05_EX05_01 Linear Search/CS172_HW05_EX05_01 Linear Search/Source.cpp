9c1f4a0047f3452f35a822554f29fa9d4c1866fd
Ian Fisher <ifisher18@my.whitworth.edu>
2016-10-26 21:46:56 -0700

    Began work- keep working on this

--- CS172_HW05_EX05_01 Linear Search/CS172_HW05_EX05_01 Linear Search/Source.cpp
+++ CS172_HW05_EX05_01 Linear Search/CS172_HW05_EX05_01 Linear Search/Source.cpp
@@ -9,6 +9,21 @@ using namespace std;
 
 // rewrite the linear search function in Listing 7.9 to use a generic type for array elements.
 // test the function...
-int main() {
-	return 0;
+
+template<typename T>
+int linearSearch(const T list[], T key, int arraySize)
+{
+	for(int i = 0; i < arraySize; i++)
+	{
+		if (key == list[i])
+			return i;
+	}
+	return -1;
+}
+
+// Test
+int main ()
+{
+
+	cout << linearSearch()
 }
