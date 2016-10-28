d55efa5f90bed37dfc8324065fa51b04cf23777f
ifisher18 <ifisher18@my.whitworth.edu>
2016-10-28 13:50:49 -0700

    Major Changes-- fix bugs

--- CS172_HW05_EX05_01 Linear Search/CS172_HW05_EX05_01 Linear Search/Source.cpp
+++ CS172_HW05_EX05_01 Linear Search/CS172_HW05_EX05_01 Linear Search/Source.cpp
@@ -13,17 +13,28 @@ using namespace std;
 template<typename T>
 int linearSearch(const T list[], T key, int arraySize)
 {
-	for(int i = 0; i < arraySize; i++)
-	{
-		if (key == list[i])
-			return i;
-	}
-	return -1;
+    for(int i = 0; i < arraySize; i++)
+    {
+        if (key == list[i])
+            return i;
+    }
+    return -1;
 }
 
 // Test
 int main ()
 {
-
-	cout << linearSearch()
+    // Arrays of int, double, and string values
+    int ArrayInts[] = {1, 2, 3, 4, 5, 6, 7};
+    double ArrayDubs[] = {2.5, 6.8, 9.76, 4.22};
+    string ArrayString [] = {'Monday' , 'Tuesday' , 'Wednesday' , 'Thursday'};
+    
+    // tests the LinearSearch function with ints
+    cout << linearSearch(ArrayInts, 5, 7) << endl;
+    
+    // tests the LinearSearch functin with doubles
+    cout << LinearSearch(ArrayDubs, 6.8, ArrayDubs.size_t) << endl;
+    
+    // tests the LinearSearch function with strings
+    cout << linearSearch(ArrayString, <#T key#>, <#int arraySize#>)
 }
