55cc18ba280dde36eac54c1ad30237148acc612b
ifisher18 <ifisher18@LAB-SCI-304-25>
2016-11-01 08:47:02 -0700

    Changes-- need help!!

--- CS172_HW05_EX05_03 Implement Vector Class/CS172_HW05_EX05_03 Implement Vector Class/Source.cpp
+++ CS172_HW05_EX05_03 Implement Vector Class/CS172_HW05_EX05_03 Implement Vector Class/Source.cpp
@@ -5,11 +5,38 @@
 //////////////////////////
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 // Implement the vector class as an exercise
 // implement only the functions defined in the UML class diagram, as shown in Figure 12.2
 
+class vector<elementType> {
+	// data field
+
+public:
+	// Constructs an empty vector with the specified element type
+	vector <elementType>() {
+		vector <elementType>;
+	}
+
+	// Constructs a vector with the initial size, filled with default values
+	vector <elementType>(size : int) {
+		vector <elementType>[size];
+	}
+
+	// Constructs a vector with the initial size, filled with specified values
+	vector <elementType>(size : int, defaultValue : elementType) {
+		
+	}
+	void push_back(element : elementType) {}
+	void pop_back() {}
+	unsigned const size() {}
+	elementType const at(index: int) {}
+	bool const empty () {}
+	void clear () {}
+	void swap(v2: vector) {}
+};
 int main() {
 	return 0;
 }
