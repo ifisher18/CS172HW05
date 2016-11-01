b00d4fe714f5dccbd777a0857261c3ceef58e062
ifisher18 <ifisher18@LAB-SCI-304-25>
2016-11-01 09:02:24 -0700

    Changes... NEED help!!

--- CS172_HW05_EX05_04 Shuffle Vector/CS172_HW05_EX05_04 Shuffle Vector/Source.cpp
+++ CS172_HW05_EX05_04 Shuffle Vector/CS172_HW05_EX05_04 Shuffle Vector/Source.cpp
@@ -10,6 +10,19 @@ using namespace std;
 // Write a function that shuffles the contents in a vector
 // write test program...
 
+// function that shuffles the contents in a vector
+template <typename T>
+void shuffle(vector<T>& v) {
+	// ?? need help to get going here...
+}
+
+// test program
 int main() {
-	return 0;
+	cout << "Enter 10 integer values into an array: ";
+	int TestVals[10];
+	for (int i = 0; i > 10; i++) { // for loop that enters vals into array
+		TestVals[i];
+	}
+
+	// next, vall shuffle function an display shuffled numbers
 }
