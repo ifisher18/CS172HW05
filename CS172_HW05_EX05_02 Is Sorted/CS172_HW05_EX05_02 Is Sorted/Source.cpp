6a8be34473ffc5f0edea054afeb96ba7856245a4
ifisher18 <ifisher18@LAB-SCI-304-25>
2016-11-01 08:25:07 -0700

    Major changes-- need help

--- CS172_HW05_EX05_02 Is Sorted/CS172_HW05_EX05_02 Is Sorted/Source.cpp
+++ CS172_HW05_EX05_02 Is Sorted/CS172_HW05_EX05_02 Is Sorted/Source.cpp
@@ -10,6 +10,48 @@ using namespace std;
 // Write function to check if the elements in the array are sorted
 // test the function
 
+template<typename T>
+
+// function that checks to see if an array is sorted
+// returns true if sorted
+bool isSorted(const T list[], int size) {
+	for (int i = 0; i < size; i++) {
+		if (list[i] > list[i + 1]) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
+
+// isSorted test
 int main() {
-	return 0;
+	
+	// Test with array of ints
+	cout << "int test: {1, 2, 3, 4, 5 }" << endl;
+	int Testints[5] = { 1, 2, 3, 4, 5 };
+	if ((isSorted(Testints, 5)) == true) {
+		cout << "Sorted!" << endl;
+	}
+	else {
+		cout << "Not sorted!" << endl;
+	}
+	cout << endl;
+
+	// Test with array of doubles
+	cout << "double test: { 2.5, 4.7, 3.5, 2.4, 1.8 }" << endl;
+	double Testdubs[5] = { 2.5, 4.7, 3.5, 2.4, 1.8 };
+	isSorted(Testdubs, 5);
+	if ((isSorted(Testdubs, 5)) == true) {
+		cout << "Sorted!" << endl;
+	}
+	else {
+		cout << "Not sorted!" << endl;
+	}
+	cout << endl;
+
+	// Test with array of strings
+	cout << "String test: {}" << endl;
+	string TestStrings[4] = {'h'}
 }
