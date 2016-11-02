c100c99cd9c97d895f1482111587dcab4cf43893
ifisher18 <ifisher18@LAB-SCI-214-16>
2016-11-01 22:04:27 -0700

    Final comments...DONE!!

--- CS172_HW05_EX05_01 Linear Search/CS172_HW05_EX05_01 Linear Search/Source.cpp
+++ CS172_HW05_EX05_01 Linear Search/CS172_HW05_EX05_01 Linear Search/Source.cpp
@@ -11,8 +11,10 @@ using namespace std;
 // test the function...
 
 template<typename T>
+// function that searches through an array for a predetermined 'key'
 int linearSearch(const T list[], T key, int arraySize)
 {
+	// for loop that loops through the array and checks if the key is equal to list[i]
     for(int i = 0; i < arraySize; i++)
     {
         if (key == list[i])
@@ -21,20 +23,29 @@ int linearSearch(const T list[], T key, int arraySize)
     return -1;
 }
 
-// Test
+// Test function for linearSearch
 int main ()
 {
-    // Arrays of int, double, and string values
+    // Test arrays of int, double, and string values
     int ArrayInts[] = {1, 2, 3, 4, 5, 6, 7};
     double ArrayDubs[] = {2.5, 6.8, 9.76, 4.22};
     string ArrayString [] = {"Monday" , "Tuesday" , "Wednesday" , "Thursday"};
     
     // tests the LinearSearch function with ints
-    cout << linearSearch(ArrayInts, 5, 7) << endl;
+	cout << "**Test 1: int ArrayInts[] = {1, 2, 3, 4, 5, 6, 7};" << endl;
+	cout << "5 is in array location " << linearSearch(ArrayInts, 5, 7) << endl;
+    cout << "(" << linearSearch(ArrayInts, 5, 7) << ")" << endl;
+	cout << endl;
     
     // tests the LinearSearch functin with doubles
-    cout << linearSearch(ArrayDubs, 6.8, 5) << endl;
+	cout << "**Test 2: double ArrayDubs[] = {2.5, 6.8, 9.76, 4.22};" << endl;
+	cout << "6.8 is in array location " << linearSearch(ArrayDubs, 6.8, 5) << endl;
+    cout << "(" << linearSearch(ArrayDubs, 6.8, 5) << ")" << endl;
+	cout << endl;
     
     // tests the LinearSearch function with strings
-	cout << linearSearch(ArrayString, string("Wednesday"), 4) << endl;
+	cout << "**Test 3: string ArrayString [] = {'Monday' , 'Tuesday' , 'Wednesday' , 'Thursday'};" << endl;
+	cout << "'Wednesday' is in array location " << linearSearch(ArrayString, string("Wednesday"), 4) << endl;
+	cout << "(" << linearSearch(ArrayString, string("Wednesday"), 4) << ")" << endl;
+	cout << endl;
 }
