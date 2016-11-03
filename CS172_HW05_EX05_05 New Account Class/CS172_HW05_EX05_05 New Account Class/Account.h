d41c220dfb6aa4becd9c0c3dab25327096115d79
ifisher18 <ifisher18@my.whitworth.edu>
2016-11-02 23:05:44 -0700

    Major updates-- almost done

--- CS172_HW05_EX05_05 New Account Class/CS172_HW05_EX05_05 New Account Class/Account.h
+++ CS172_HW05_EX05_05 New Account Class/CS172_HW05_EX05_05 New Account Class/Account.h
@@ -1,16 +1,40 @@
-#pragma once
+#ifndef
+#define
 
 #include <iostream>
 using namespace std;
 
-class Account {
+// creates a class called Account
+// all public
+class Account
+{
 public:
 	// data field
-	int id;
-	double balance;
-	double annualInterestRate;
-	Account();
-	// accessor and mutator functions for id, balance, and annualInterestRate
-	double withdraw(double amount);
-	double deposit(double amount);
+    int id;
+    double balance;
+    double annualInterestRate;
+    string name;
+    
+    
+    Account(int, double, double);
+    Account();
+    
+	// get functions that return either string, double, or int
+    string getname();
+    double getbalance();
+    double getannualInterestRate();
+    int getid();
+    
+    void set_name(string);
+    void set_balance(double);
+    void set_annualInterestRate(double); // use algorithm
+    void set_id(int);
+    
+    int getMonthlyInterestRate();
+	// functions that take into account the balance in relation to amounts withdrawn and deposited
+    void withdraw(int);
+    void deposit(int);
+    
 };
+
+#endif
